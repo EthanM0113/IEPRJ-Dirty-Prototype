6bea77c27d42db042afb8ae964c0e2208c13049b
Dysrado <63017345+Dysrado@users.noreply.github.com>
2022-12-12 19:45:45 +0800

    Fixed merge

---
+++ Assets/Scripts/Player/PlayerHearts.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHearts : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int numOfHearts;
+    [SerializeField] private List<Image> hearts;
+
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for(int i = 0; i < hearts.Count; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    public void DamagePlayer(int dmg)
+    {
+        health -= dmg;
+    }
+
+}
