8976810ec899127109d732f00bb0d2feafaf5e5a
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-09-27 22:17:40 +0800

    Bug Fixes
    Bugs:
    - Jumping from enemies
    - Wisp body being able to be pushed by the player
    - Respawning of enemies
    - Fix Tutorial Text to be more concise
    - At low fuel, the flame separates from the bar.
    
    Added Features:
    - Skip tutorial button

--- Assets/Scripts/MainMenu/MainMenuHandler.cs
+++ Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     [SerializeField] private AudioClip buttonSound;
     public void PlayGame() {
         Debug.Log("PlayGame");
         SoundManager.Instance.PlaySound(buttonSound);
         Time.timeScale = 1;
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void PlayTutorial()
+    {
+        Debug.Log("PlayGame");
+        SoundManager.Instance.PlaySound(buttonSound);
+        Time.timeScale = 1;
         SceneManager.LoadScene("TutorialLevel");
+    }
+
+    public void OpenTutorialMenu()
+    {
+        anim.SetTrigger("OpenTutorial");
     }
 
     public void ExitGame() {
