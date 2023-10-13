a5afbaf56fd0526d86140fc8bfa3ca7844c94e0b
Dysrado <rajjardin@gmail.com>
2023-10-13 21:57:38 +0800

    Added puzzle rooms

--- Assets/Scripts/RoomGeneration/RoomOptimization.cs
+++ Assets/Scripts/RoomGeneration/RoomOptimization.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class RoomOptimization : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class RoomOptimization : MonoBehaviour
     [SerializeField] private bool HasEnemy = false;
     [SerializeField] private LayerMask PlayerLayer;
     private BaseEnemy[] EnemyContainer;
-
+    private Transform[] childList;
     private float DetectionRadius = 25.0f;
     private bool HasExplored = false;
     
@@ -26,11 +27,6 @@ public class RoomOptimization : MonoBehaviour
 
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.yellow;
-    //    Gizmos.DrawWireSphere(transform.position, DetectionRadius);
-    //}
     
     private void Update()
     {
@@ -38,7 +34,11 @@ public class RoomOptimization : MonoBehaviour
         {
             EnemyContainer = gameObject.GetComponentsInChildren<BaseEnemy>();
         }
-        //childList = gameObject.GetComponentsInChildren<Transform>(true);
+        else if (!HasEnemy)
+        {
+            childList = gameObject.GetComponentsInChildren<Transform>(true);
+
+        }
         Collider[] Player = Physics.OverlapSphere(transform.position, DetectionRadius, PlayerLayer);
         if (Player.Length == 0) //If there is no player nearby
         {
@@ -48,13 +48,16 @@ public class RoomOptimization : MonoBehaviour
                 {
                     EnemyContainer[i].PauseEnemy();
                 }
-            }
-           
 
-            //for (int i = 2; i < childList.Length; i++)
-            //{
-            //    childList[i].gameObject.SetActive(false);
-            //}
+            }
+            else if (!HasEnemy)
+            {
+                for (int i = 3; i < childList.Length; i++)
+                {
+                    childList[i].gameObject.SetActive(false);
+                }
+            }
+            
             IsActive = false;
             return;
         }
@@ -67,134 +70,38 @@ public class RoomOptimization : MonoBehaviour
             }
 
         }
-        
-        //for (int i = 0; i < childList.Length; i++)
-        //{
-        //    childList[i].gameObject.SetActive(true);
-        //}
-
-
+        else if (!HasEnemy)
+        {
+            for (int i = 3; i < childList.Length; i++)
+            {
+                childList[i].gameObject.SetActive(true);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //if (!IsActive) {
-                IsActive = true;
-                //ActivateRooms();
-                GameObject.FindGameObjectWithTag("MapNode").transform.position = MapNode.transform.position;
-                if (!HasExplored && NeedsIcon)
+            GameObject.FindGameObjectWithTag("MapNode").transform.position = MapNode.transform.position;
+            if (!HasExplored && NeedsIcon)
             {
                 MapNode.transform.GetChild(0).gameObject.SetActive(true);
             }
-
-            //}
-            //else if (IsActive) //Moving to adjacent room
-            //{
-            //    HasPlayer = true;
-            //    IsActive = true;
-            //}
-            
-        }
-
-        //if (collision.gameObject.CompareTag("RoomCollider") || collision.gameObject.CompareTag("StartingRoom"))
-        //{
-        //    Debug.Log("Room Detected");
-        //    AdjRoomsA.Add(collision.gameObject);
-        //}
         
+        } 
     }
 
-    private void OnCollisionStay(Collision collision)
+
+
+    /*private void OnCollisionExit(Collision collision)
     {
-        //if (collision.gameObject.CompareTag("RoomCollider") || collision.gameObject.CompareTag("StartingRoom"))
-        //{
-        //    Debug.Log("Room Detected");
-        //}
-        //if (collision.gameObject.CompareTag("RoomCollider") ||collision.gameObject.CompareTag("StartingRoom"))
-        //{//Activate the room the player is currently in
-        //    if (&& collision.gameObject.GetComponent<RoomOptimization>().IsActive == false)
-        //    {
-        //        collision.gameObject.GetComponent<RoomOptimization>().ActivateRoom();
 
-        //    }
-            
-        //    else if (!IsActive && collision.gameObject.GetComponent<RoomOptimization>().IsActive == true)
-        //    {
-        //        collision.gameObject.GetComponent<RoomOptimization>().DeactivateRoom();
-        //    }
-        //}
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        //if (IsActive)
-        //{
-        //    if (collision.gameObject.CompareTag("Player"))
-        //    {
-        //        HasPlayer = false;
-        //        for (int i = 0; i < AdjRoomsA.Count - 1; i++) { 
-        //            //Checks which room the player went to
-        //            if (AdjRoomsA[i].GetComponent<RoomOptimization>().HasPlayer == true)
-        //            {
-
-        //            }
-        //        }
-        //    }
-        //}
         if (collision.gameObject.CompareTag("Player"))
         {
             GameObject.FindGameObjectWithTag("RespawnNode").transform.position = collision.gameObject.transform.position;
         }
 
-    }
-   
-    //void ActivateRooms()
-    //{
-    //    for (int i = 0; i < AdjRoomsA.Count - 1; i++)
-    //    {
-    //        AdjRoomsA[i].gameObject.GetComponent<RoomOptimization>().ActivateRoom();
-    //    }
-    //}
+    }*/
 
-    //void DeactivateRooms(List<GameObject> AdjRoomsB)
-    //{
-    //    List<GameObject> AdjRooms = new List<GameObject>();
-    //    //AdjRoomsB is the list of adjacent rooms of the room the player has moved to. AdjRoomsA is the list of adjacent rooms of the room the player has left
-    //    for (int i = 0; i < AdjRoomsA.Count - 1; i++)
-    //    {
-    //        bool isAdjacent = false;
-    //        for (int j = 0; j < AdjRoomsB.Count - 1; j++)
-    //        {
-    //            if (AdjRooms[i] == AdjRoomsB[j]) //Room in between Room A and B
-    //            {
-    //                isAdjacent = true;
-    //            }
-    //        }
-    //        if (isAdjacent)
-    //        {
-    //            AdjRooms.Add(AdjRoomsA[i]);
-    //        }
-            
-    //    }
-    //    for (int i = 0;i < AdjRooms.Count - 1; i++)
-    //    {
-    //        AdjRooms[i].GetComponent<RoomOptimization>().DeactivateRoom();
-    //    }
-    //}
-
-    //public List<GameObject> GetAdjRooms()
-    //{
-    //    return AdjRoomsA;
-    //}
-    //public void ActivateRoom()
-    //{
-    //    IsActive = true;
-    //}
-
-    //public void DeactivateRoom()
-    //{
-    //    IsActive = false;
-    //}    
 }
