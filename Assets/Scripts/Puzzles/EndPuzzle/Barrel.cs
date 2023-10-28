a24ca750b8e120827f43ac479d73e71f932d6669
Rizumuwu <miguel_paglinawan@dlsu.edu.ph>
2023-10-28 13:52:01 +0800

    Inventory text application

--- Assets/Scripts/Puzzles/EndPuzzle/Barrel.cs
+++ Assets/Scripts/Puzzles/EndPuzzle/Barrel.cs
@@ -66,4 +66,27 @@ public class Barrel : MonoBehaviour
 
         }
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            switch (state)
+            {
+                case 0:
+                    barrel.GetComponent<LoreObject>().SetText("It looks mechanical, there must be a trigger somewhere.");
+                    barrel.GetComponent<Renderer>().material.color = Color.gray;
+                    break;
+                case 1:
+                    barrel.GetComponent<LoreObject>().SetText("It's red hot!");
+                    barrel.GetComponent<Renderer>().material.color = Color.red;
+                    break;
+                case 2:
+                    barrel.GetComponent<LoreObject>().SetText("Cold to the touch. It can even freeze water.");
+                    barrel.GetComponent<Renderer>().material.color = Color.cyan;
+                    break;
+
+            }
+        }
+    }
 }
--- Assets/Scripts/Puzzles/EndPuzzle/PlayerInventory.cs
+++ Assets/Scripts/Puzzles/EndPuzzle/PlayerInventory.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
+    [SerializeField] private GameObject inventoryText;
+
     private bool water;
     private bool chalice;
     private bool rustedKey;
@@ -21,31 +24,59 @@ public class PlayerInventory : MonoBehaviour
         iceChunks = false;
         freezingScroll = false;
         grandDoorGear = false;
+
+        inventoryText.GetComponent<TextMeshProUGUI>().text = "Inventory: \n";
     }
 
-    public void SetWater(bool hasWater) { water = hasWater; Debug.Log("Got water!"); }
+    private void UpdateList()
+    {
+        inventoryText.GetComponent<TextMeshProUGUI>().text = "Inventory: \n";
+
+        if (rustedKey)
+            inventoryText.GetComponent<TextMeshProUGUI>().text += "Rusted Key\n";
+        if(moldyBread > 0)
+        {
+            for (int i = 0; i < moldyBread; i++)
+                inventoryText.GetComponent<TextMeshProUGUI>().text += "MoldyBread\n";
+        }
+        if(iceChunks)
+            inventoryText.GetComponent<TextMeshProUGUI>().text += "Chunks of Ice\n";
+        if(freezingScroll)
+            inventoryText.GetComponent<TextMeshProUGUI>().text += "Scroll of Freezing\n";
+        if (chalice)
+        {
+            if(water)
+                inventoryText.GetComponent<TextMeshProUGUI>().text += "Chalice full of water\n";
+            else
+                inventoryText.GetComponent<TextMeshProUGUI>().text += "Empty Chalice\n";
+        }
+        if(grandDoorGear)
+            inventoryText.GetComponent<TextMeshProUGUI>().text += "Grand Door Gear\n";
+    }
+
+    public void SetWater(bool hasWater) { water = hasWater; UpdateList(); }
 
     public bool GetWater() { return water; }
 
-    public void SetChalice(bool hasChalice) {  chalice = hasChalice; Debug.Log("Got chalice!"); }
+    public void SetChalice(bool hasChalice) {  chalice = hasChalice; UpdateList(); }
 
     public bool GetChalice() { return chalice; }
 
-    public void SetKey(bool hasKey) { rustedKey  = hasKey; Debug.Log("Got rusted key!"); }
+    public void SetKey(bool hasKey) { rustedKey  = hasKey; UpdateList(); }
 
     public bool GetKey() { return rustedKey; }
 
-    public void SetBread(int currBread) { moldyBread = currBread; Debug.Log("Got " + moldyBread + " bread!"); }
+    public void SetBread(int currBread) { moldyBread = currBread; UpdateList(); }
 
-    public void SetIceChunks(bool hasIceChunks) {  iceChunks = hasIceChunks; Debug.Log("Got ice chunks!"); }
+    public void SetIceChunks(bool hasIceChunks) {  iceChunks = hasIceChunks; UpdateList(); }
 
     public bool GetIceChunks() { return iceChunks; }
 
-    public void SetFreezingScroll(bool hasFreezingScroll) { freezingScroll = hasFreezingScroll; Debug.Log("Got freezing scroll!"); }
+    public void SetFreezingScroll(bool hasFreezingScroll) { freezingScroll = hasFreezingScroll; UpdateList(); }
 
     public bool GetScroll() {  return freezingScroll; }
 
-    public void SetGear(bool hasGear) { grandDoorGear = hasGear; Debug.Log("Got gear!"); }
+    public void SetGear(bool hasGear) { grandDoorGear = hasGear; UpdateList(); }
 
     public bool GetGear() {  return grandDoorGear; }
 }
